27d9a314a500cb0323a13fd6523575e90fc26c05
xuhaoran.xhr <xuhaoran.xhr@alibaba-inc.com>
2025-06-10 00:11:57 +0800

    first commit

---
+++ src/main/java/com/kuri/backend/KuriApplication.java
@@ -0,0 +1,13 @@
+package com.kuri.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kuri/backend/KuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuri.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
